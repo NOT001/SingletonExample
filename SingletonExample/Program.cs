bd21fec357923f5a104d223d68c3e4003ac259c0
NguyenHa <hnha.20it2@vku.udn.vn>
2023-09-08 08:29:53 +0700

    Thông điệp cam kết

---
+++ SingletonExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    public sealed class Singleton
+    {
+        private static Singleton instance = null;
+        private static readonly object padlock = new object();
+
+        Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Singleton instance is doing something!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.Instance.DoSomething();
+        }
+    }
+}
